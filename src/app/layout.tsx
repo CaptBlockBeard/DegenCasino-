3b6e06c3809e803e15cbf49079c9096743eb9db3
onaboat <onaboatatsea99@gmail.com>
2024-08-25 21:53:28 +1000

    fixing doctype

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,19 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Degen Casino Blink - Captain Blockbeard's Treasure Hunt",
-  description: "Set sail and test yer luck in this swashbucklin Solana token swap adventure! Spin the wheel and see if fortune favors yer quest for treasure, matey! Arrr!",
-  openGraph: {
-    title: "Degen Casino Blink - Captain Blockbeard's Treasure Hunt",
-    description: "Set sail and test yer luck in this swashbucklin Solana token swap adventure! Spin the wheel and see if fortune favors yer quest for treasure, matey! Arrr!",
-    images: [
-      {
-        url: "https://degencasinoblink.captainblockbeard.site/og-image.jpg",
-      },
-    ],
-    url: "https://degencasinoblink.captainblockbeard.site",
-    type: "website",
-  },
+  description: "Set sail and test yer luck in this swashbucklin Solana token swap adventure! Spin the wheel and see if fortune favors yer quest for treasure, matey! Arrrrrrrrr",
 };
+
+
 
 export default function RootLayout({
   children,
